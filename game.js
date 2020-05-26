10b9c632c644de20973be4a0fa4fe53eaec93de3
gvasquez11 <vasquez.greg16@gmail.com>
2020-05-25 21:39:33 -0700

    added array of cards

---
+++ game.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded',() =>{
+
+    //Creating an Array of Cards to use. Two for each card
+    const carsArray = [
+        {
+            name: 'batman',
+            image: 'images/batman.png'
+        },
+        {
+            name: 'batman',
+            image: 'images/batman.png'
+        },
+        {
+            name: 'Goku',
+            image: 'images/Goku.png'
+        },
+        {
+            name: 'Goku',
+            image: 'images/Goku.png'
+        },
+        {
+            name: 'ironman',
+            image: 'images/ironman.png'
+        },
+        {
+            name: 'ironman',
+            image: 'images/ironman.png'
+        },
+        {
+            nane: 'naruto',
+            image: 'images/naruto.jpg'
+        },
+        {
+            nane: 'naruto',
+            image: 'images/naruto.jpg'
+        },
+        {
+            name: 'superman',
+            image: 'images/superman.jpg'
+        },
+        {
+            name: 'superman',
+            image: 'images/superman.jpg'
+        },
+        {
+            name: 'todHead',
+            image: 'images/todHead.png'
+        }
+    ]
+
+})
